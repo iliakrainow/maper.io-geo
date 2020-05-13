3ad7cbe32e794d18f86dc3ffa0c1d7841808d31d
iliakrainow <40818918+iliakrainow@users.noreply.github.com>
2020-05-13 19:00:41 +0400

    lines

--- flask_app.py
+++ flask_app.py
@@ -2,20 +2,31 @@ from flask import Flask, render_template, request, jsonify
 import requests
 
 
+def append_coords(to, new):
+    if new not in to:
+        to.append(new)
+    return to
+
+
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "maper.io geo"
 
 
-
-
-
-@app.route("/", methods=['GET', 'POST'])
+@app.route("/", methods=["GET", "POST"])
 def index():
-    if request.method == 'GET':
+    global coords
+    if request.method == "GET":
         return render_template("index.html")
     else:
         a = request.form
-        params = {'ll': ','.join([a['lon'], a['lat']]), 'l': 'map', 'pt': ','.join([a['lon'], a['lat'], 'ya_ru'])}
-        return requests.get('https://static-maps.yandex.ru/1.x/', params=params).url
+        coords = append_coords(coords, a["lon"] + "," + a["lat"])
+        params = {
+            "ll": ",".join([a["lon"], a["lat"]]),
+            "l": "map",
+            "pt": ",".join([a["lon"], a["lat"], "ya_ru"]),
+            "pl": ",".join(coords),
+        }
+        return requests.get("https://static-maps.yandex.ru/1.x/", params=params).url
 
 
+coords = []
